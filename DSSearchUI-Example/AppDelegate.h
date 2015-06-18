6e68e456b49c986bb97ae259bb6c0206ec9e82fd
Deborshi Saha <deborshi.saha@gmail.com>
2015-06-18 01:26:35 -0700

    Initial Commit

---
+++ DSSearchUI-Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DSSearchUI-Example
+//
+//  Created by Deborshi Saha on 6/18/15.
+//  Copyright (c) 2015 Deborshi Saha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DSSearchUI-Example/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  DSSearchUI-Example
+//
+//  Created by Deborshi Saha on 6/18/15.
+//  Copyright (c) 2015 Deborshi Saha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
