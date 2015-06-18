75a0434582e89aa1c25ed916dd2b20dceb9005b4
Deborshi Saha <deborshi.saha@gmail.com>
2015-06-18 02:09:32 -0700

    Adding Geocoding API key

---
+++ DSSearchUI-Example/DSSearchUI/GeocodingAPIKey.h
@@ -0,0 +1,14 @@
+//
+//  GeocodingAPIKey.h
+//  DSSearchUI-Example
+//
+//  Created by Deborshi Saha on 6/18/15.
+//  Copyright (c) 2015 Deborshi Saha. All rights reserved.
+//
+
+#ifndef DSSearchUI_Example_GeocodingAPIKey_h
+#define DSSearchUI_Example_GeocodingAPIKey_h
+
+static NSString * const APIKey = @"";
+
+#endif
